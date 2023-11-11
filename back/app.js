f7fcb7e7217b93f6aec9b8c1a2fd7152e1003387
willowsenator <willowsenator@gmail.com>
2023-11-11 01:34:10 +0100

    Add endpoint to getBalance, tx, block, and currentBlockNumber

--- back/app.js
+++ back/app.js
@@ -1,19 +1,77 @@
 const express = require("express");
 const app = express();
+const {Web3} = require("web3");
 
 const PORT = 4000;
 
-app.get("/", (req, res)=>{
-    return res.send("<h1>HOME</h1>").sendStatus(200);
+const MUMBAI_RPC = "https://polygon-mumbai.g.alchemy.com/v2/FX3qHoCqEqesrN33uw-DmFEf70Jh3UiO";
+
+const web3 = new Web3(MUMBAI_RPC);
+
+
+app.get("/", async (req, res)=>{
+    try{
+        const block = (await web3.eth.getBlockNumber()).toString();
+        res.send({block}).status(200);
+    } catch(ex){
+        res.send({error:ex.message}).send(500);
+    }
 });
 
-app.get("/getTransaction", (req, res)=>{
-    return res.send("<h1>TRANSACTION</h1>").sendStatus(200);
+app.get("/block/:block", async (req, res)=>{
+    try{
+        const block = (await web3.eth.getBlock(req.params.block));
+        const blockDetails = {
+            difficulty: block.difficulty.toString(),
+            baseFeePerGas: block.baseFeePerGas.toString(),
+            miner: block.miner.toString(),
+            gasLimit: block.gasLimit.toString(),
+            nonce: block.nonce.toString(),
+            size: block.size.toString()
+        }
+        res.send(blockDetails).status(200);
+    } catch(ex){
+        res.send({error: ex.message}).status(500);
+    }
+    
 });
 
-app.get("/getBalance", (req, res)=>{
-    return res.send("<h1>BALANCE</h1>").sendStatus(200);
+app.get("/tx/:tx", async (req, res) => {
+    try {
+        const tx = await web3.eth.getTransaction(req.params.tx);
+
+        const txDetail = {
+            gas: tx.gas.toString(),
+            hash: tx.hash.toString(),
+            chainId: tx.chainId.toString(),
+            gasPrice: tx.gasPrice.toString(),
+            maxFeePerGas: tx.maxFeePerGas.toString(),
+            value: tx.value.toString()
+        }
+        res.status(200).send(txDetail);
+    } catch (ex) {
+        console.error(ex);
+        res.status(500).send({ error: ex.message });
+    }
 });
+
+
+app.get("/balance/:account", async (req, res) => {
+    try {
+        const account = req.params.account;
+        const balanceWei = (await web3.eth.getBalance(account)).toString();
+        const balanceEther = web3.utils.fromWei(balanceWei, "ether").toString();
+
+        console.log(`Balance in Wei: ${balanceWei}`);
+        console.log(`Balance in Ether: ${balanceEther}`);
+
+        res.status(200).send({ balanceWei, balanceEther });
+    } catch (ex) {
+        console.error(ex);
+        res.status(500).send({ error: ex.message });
+    }
+});
+
 
 app.listen(PORT, ()=>{
     console.log("Listening in %s", PORT);
