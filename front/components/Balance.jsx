9cd32b11b7f11429226debd33d544204c6f5f16f
willowsenator <willowsenator@gmail.com>
2023-11-11 20:08:24 +0100

    Connect backend and front with the endpoints

--- front/components/Balance.jsx
+++ front/components/Balance.jsx
@@ -1,7 +1,30 @@
 import {useParams} from 'react-router-dom'
+import {useQuery} from 'react-query'
+
+const getAccount = async(account) => {
+    try{
+        const response = await fetch(`http://localhost:4000/balance/${account}`);
+        const data = await response.json();
+        return data;
+    } catch(ex){
+        return ex.message;
+    }
+}
+
 export const Balance = () => {
     const params = useParams();
-    return <div>
-        <h1>Address: {params.account}</h1>
-    </div>
+    const {isLoading, isError, data} = useQuery(["account", params.account],()=> getAccount(params.account));
+
+    if(isLoading){
+        return <h1>Loading...</h1>;
+    }
+    if(isError){
+        return <h1>{data}</h1>;
+    } else{
+        return <div>
+            <h1>Account: {params.account}</h1>
+            <h2>Balance in Wei: {data.balanceWei}</h2>
+            <h2>Balance in Ether: {data.balanceEther} Eth</h2>
+        </div>;
+    }
 };
--- front/components/Block.jsx
+++ front/components/Block.jsx
@@ -1,8 +1,34 @@
 import {useParams} from 'react-router-dom'
+import {useQuery} from 'react-query'
+
+const getBlock = async(blockNumber) => {
+    try{
+        console.log(`BlockNumber: ${blockNumber}`)
+        const res = await fetch(`http://localhost:4000/block/${blockNumber}`);
+        const data = await res.json();
+        console.log(blockNumber);
+        console.log(data);
+        return data;
+    } catch(ex){
+        return ex.message;
+    }
+}
+
 export const Block = () => {
     const params = useParams();
     console.log(params);
+    
+    const {isLoading, isError, data} = useQuery(["blockNumber", params.blockNumber], () => getBlock(params.blockNumber));
+
+    if(isLoading){
+        return <h1>Loading...</h1>
+    }
+    if(isError){
+        return <h1>{data}</h1>
+    }
+
     return <div>
         <h1>Block: {params.blockNumber}</h1>
+        {JSON.stringify(data,null,4)}
     </div>
 };
